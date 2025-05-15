6d2676e6e689b9732aa2e84dbebb4519b33643b9
RobsonTrasel <robson.trasel@gmail.br>
2025-05-14 23:37:29 -0300

    lint(route): prettier working here ;)

--- src/infrastructure/web/routes/webhook.route.ts
+++ src/infrastructure/web/routes/webhook.route.ts
@@ -1,18 +1,17 @@
-import {Router} from 'express';
-import {AppContainer} from "../../../container/app.container";
-
+import { Router } from "express";
+import { AppContainer } from "../../../container/app.container";
 
 const router = Router();
 const app = new AppContainer();
 
-router.post('/', async (req, res) => {
-    try {
-        await app.controller.handle(req.body);
-        res.sendStatus(200);
-    } catch (err) {
-        console.error('❌ Erro no webhook:', err);
-        res.sendStatus(500);
-    }
+router.post("/", async (req, res) => {
+  try {
+    await app.controller.handle(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error("❌ Erro no webhook:", err);
+    res.sendStatus(500);
+  }
 });
 
 export default router;
